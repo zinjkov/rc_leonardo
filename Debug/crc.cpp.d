ea1cc1e095869bd6789ea5a8efdafec95163c39b
zinjkov <zinjkov.su@gmail.com>
2018-06-02 21:59:44 +1000

    Revert "init"
    This reverts commit fa696d7736d314ab90849d0f4c02a50ad78d6b7a.

--- Debug/crc.cpp.d
+++
@@ -1,3 +0,0 @@
-C:\Users\zinjk\AppData\Local\Temp\VMBuilds\rc_leonardo\leonardo\Debug\crc.cpp.o: \
- C:\Users\zinjk\AppData\Local\Temp\VMBuilds\rc_leonardo\leonardo\Debug\crc.cpp \
- C:\Users\zinjk\AppData\Local\Temp\VMBuilds\rc_leonardo\leonardo\Debug\crc.hpp
--- base_packet_t.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by zinjkov on 05.04.18.
-//
-
-#include "base_packet_t.hpp"
-
-namespace rov_types {
-
-}
